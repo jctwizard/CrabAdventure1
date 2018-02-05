36005d0a40f8293f4bac799c2ec150f7ba559630
James Wood <jamie_wood@ymail.com>
2018-02-05 19:33:39 +0000

    Add initial project

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+
+	Vector3 initialScale;
+	public GameObject bubble;
+
+	// Use this for initialization
+	void Start () {
+		initialScale = transform.localScale;
+		bubble.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward,
+		                 Camera.main.transform.rotation * Vector3.up);
+		transform.localScale = initialScale + Vector3.up * Mathf.Sin(Time.time) * 0.05f + Vector3.right * Mathf.Cos(Time.time) * 0.05f;
+		
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.tag == "Player")
+		{
+			bubble.SetActive(true);
+			Debug.Log("trigger!");
+		}
+	}
+
+	void OnTriggerExit(Collider collider)
+	{
+		if (collider.tag == "Player")
+		{
+			bubble.SetActive(false);
+			Debug.Log("no trigger!");
+		}
+	}
+}
